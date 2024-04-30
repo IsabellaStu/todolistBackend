01868a1db7ebee7af6168bd8ec0be5c1fdf11343
IsabellaStu <isabellasturniolo1996@outlook.it>
2024-05-01 00:17:33 +0200

    Aggiunti attributi alla classe User

--- todolist/src/main/java/com/example/model/User.java
+++ todolist/src/main/java/com/example/model/User.java
@@ -1,15 +1,81 @@
 package com.example.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column
+    private String nome;
+    @Column
+    private String cognome;
+    @Column
+    private String email;
+    @OneToOne
+    Credenziali credenziali;
 
+    public User(){
 
+    }
+
+    public User(long id, String nome, String cognome, String email, Credenziali credenziali) {
+        this.id = id;
+        this.nome = nome;
+        this.cognome = cognome;
+        this.email = email;
+        this.credenziali = credenziali;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCognome() {
+        return cognome;
+    }
+
+    public void setCognome(String cognome) {
+        this.cognome = cognome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Credenziali getCredenziali() {
+        return credenziali;
+    }
+
+    public void setCredenziali(Credenziali credenziali) {
+        this.credenziali = credenziali;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cognome='" + cognome + '\'' +
+                ", email='" + email + '\'' +
+                ", credenziali=" + credenziali +
+                '}';
+    }
 }
