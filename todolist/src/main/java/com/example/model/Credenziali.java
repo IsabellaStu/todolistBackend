ef45245a5a9c9c22c7e8951d3e3b4bef44576b9a
IsabellaStu <isabellasturniolo1996@outlook.it>
2024-05-01 00:13:25 +0200

    Aggiunti attributi alla classe credenziali

--- todolist/src/main/java/com/example/model/Credenziali.java
+++ todolist/src/main/java/com/example/model/Credenziali.java
@@ -12,5 +12,46 @@ public class Credenziali {
     @Column
     private String password;
 
+    public Credenziali(){
 
+    }
+
+    public Credenziali(long id, String username, String password) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "Credenziali{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
 }
