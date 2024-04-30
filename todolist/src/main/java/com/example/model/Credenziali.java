70cd4cd1bbb8e6f369de7a55e93f058b0f69456e
IsabellaStu <isabellasturniolo1996@outlook.it>
2024-05-01 00:12:06 +0200

    Aggiunti attributi alla classe credenziali

--- todolist/src/main/java/com/example/model/Credenziali.java
+++ todolist/src/main/java/com/example/model/Credenziali.java
@@ -1,15 +1,16 @@
 package com.example.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
 public class Credenziali {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+    @Column
+    private String username;
+    @Column
+    private String password;
 
 
 }
